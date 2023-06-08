b5758ec836812cf357c9c7f7efe1566e5299bc33
rkwanderon <rajnikant@wanderon.in>
2023-06-08 15:16:35 +0530

    line 22 updated

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp; Rajnikant
             <code className={styles.code}>pages/index.js</code>
-            <p>I'm pro developer</p>
+            <p>I'm pro rajnikant developer</p>
           </p>
           <div>
             <a
